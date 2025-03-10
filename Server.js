5526ed138e4eb3b66ed4899ee87681511f6dc1fa
Bonaeineza31 <b.ineza@alustudent.com>
2025-03-10 23:29:36 +0200

    few changes

--- Server.js
+++ Server.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// Server.js
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoutes from './Routes/AuthRoute.js';
+import userRoutes from './Routes/UserRoute.js';
+import appointmentRoutes from './Routes/AppointmentRoute.js';
+import healthDataRoutes from './Routes/DataRoute.js';
+
+// Load environment variables
+dotenv.config();
 
 const db_user = process.env.DB_USER;
 const db_name = process.env.DB_NAME;
 const db_pass = process.env.DB_PASS;
-
-// Load environment variables
-dotenv.config();
 
 // Create Express app
 const app = express();
@@ -17,17 +22,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const dbUri = `mongodb+srv://${db_user}:${encodeURIComponent(db_pass)}@cluster0.qfmve.mongodb.net/${db_name}?retryWrites=true&w=majority&appName=Cluster0`
+const dbUri = `mongodb+srv://${db_user}:${encodeURIComponent(db_pass)}@cluster0.qfmve.mongodb.net/${db_name}?retryWrites=true&w=majority&appName=Cluster0`;
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(dbUri)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
-
-// Import routes
-const authRoutes = require('./Routes/AuthRoute');
-const userRoutes = require('./Routes/UserRoute');
-const appointmentRoutes = require('./Routes/AppointmentRoute');
-const healthDataRoutes = require('./Routes/DataRoute');
 
 // Use routes
 app.use('/api/auth', authRoutes);
