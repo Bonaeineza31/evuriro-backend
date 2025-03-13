16ad723099576c0bc7d5578cc5afb038b3c45196
Bonaeineza31 <b.ineza@alustudent.com>
2025-03-13 07:39:47 +0200

    added the usermodel

---
+++ Models/UserModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const { model, Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      required: false,
+      default: "patient",
+      enum: ["patient", "doctor", "admin"]
+    },
+    tokens: {
+      token: {
+        type: String,
+        default: ""
+      }
+    }
+  },
+  { timestamps: true }
+);
+
+// Export the model
+const User = model("User", userSchema);
+export default User;
