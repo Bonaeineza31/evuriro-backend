93803e06d5723dadd2d97d56cf0ef9cc32a90ee7
Bonaeineza31 <b.ineza@alustudent.com>
2025-03-20 12:23:32 +0200

    fixed port

--- Server.js
+++ Server.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 });
 
 if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5006;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
